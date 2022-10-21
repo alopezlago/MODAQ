b262b536cd2c89f4872eeacdf947bfe66cc96d59
Alejandro Lopez-Lago <alopezlago@cs.hmc.edu>
2022-10-21 07:20:20 +0000

    Merged PR 256: 1.10.1 - Update Google Sheets sign-in
    - Update to Google's new authentication library since the old one will be deprecated by early 2023
    - Bump version to 1.10.1

--- src/sheets/SheetsApi.ts
+++ src/sheets/SheetsApi.ts
@@ -3,6 +3,8 @@ import { LoadingState } from "../state/SheetState";
 import { UIState } from "../state/UIState";
 import { ISheetsApi, ISheetsBatchGetResponse, ISheetsGetResponse } from "./ISheetsApi";
 
+const timedOutMessage = "Timed out signing into Google Sheets";
+
 export const SheetsApi: ISheetsApi = {
     initializeIfNeeded: async (uiState: UIState): Promise<void> => {
         if (
@@ -13,29 +15,66 @@ export const SheetsApi: ISheetsApi = {
         }
 
         // Bit of a hacky wait to wait until the callback is done
+        // Need to follow this approach: https://developers.google.com/identity/oauth2/web/guides/migration-to-gis#gapi-asyncawait
         uiState.sheetsState.setSheetsApiInitialized(LoadingState.Loading);
-        const promise: Promise<void> = new Promise<void>((resolve, reject) => {
-            gapi.load("client:auth2", async () => {
-                try {
-                    await gapi.client.init({
-                        clientId: uiState.sheetsState.clientId,
-                        discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
-                        scope: "https://www.googleapis.com/auth/spreadsheets",
-                    });
 
-                    const authInstance: gapi.auth2.GoogleAuth = gapi.auth2.getAuthInstance();
-                    const isSignedIn: boolean = authInstance.isSignedIn.get();
-                    if (!isSignedIn) {
-                        await authInstance.signIn();
-                    }
+        const promise: Promise<void> = new Promise<void>(async (resolve, reject) => {
+            try {
+                // Load gapi first, then load the GIS client
+                await new Promise<void>((resolve) => {
+                    gapi.load("client", resolve);
+                });
 
-                    uiState.sheetsState.setSheetsApiInitialized(LoadingState.Loaded);
-                    resolve();
-                } catch (error) {
-                    uiState.sheetsState.setSheetsApiInitialized(LoadingState.Error);
-                    reject(error);
+                // Typings issue, init isn't listed as a method
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                await (gapi.client as any).init({});
+
+                await gapi.client.load("https://sheets.googleapis.com/$discovery/rest?version=v4");
+
+                const clientId: string | undefined = uiState.sheetsState.clientId;
+                if (clientId == undefined) {
+                    reject(new Error("No clientId specified"));
+                    return;
                 }
-            });
+
+                // If the user hasn't done anything after 2 minutes, go back to keeping this uninitialized so they can
+                // sign in again
+                const cancelSignIn = setTimeout(() => {
+                    if (uiState.sheetsState.apiInitialized === LoadingState.Loading) {
+                        uiState.sheetsState.setSheetsApiInitialized(LoadingState.Unloaded);
+                        uiState.sheetsState.setRosterLoadStatus(
+                            {
+                                isError: false,
+                                status: timedOutMessage,
+                            },
+                            LoadingState.Unloaded
+                        );
+                        reject(timedOutMessage);
+                    }
+                }, 2 * 60 * 1000);
+
+                const tokenClient = google.accounts.oauth2.initTokenClient({
+                    client_id: clientId,
+                    scope: "https://www.googleapis.com/auth/spreadsheets",
+                    callback: (tokenResponse) => {
+                        clearTimeout(cancelSignIn);
+
+                        if (tokenResponse.error != null) {
+                            uiState.sheetsState.setSheetsApiInitialized(LoadingState.Error);
+                            reject(tokenResponse.error_description);
+                            return;
+                        }
+
+                        uiState.sheetsState.setSheetsApiInitialized(LoadingState.Loaded);
+                        resolve();
+                    },
+                });
+
+                tokenClient.requestAccessToken();
+            } catch (error) {
+                uiState.sheetsState.setSheetsApiInitialized(LoadingState.Error);
+                reject(error);
+            }
         });
 
         await promise;
