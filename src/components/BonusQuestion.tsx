7e23c360520bb15ccd7c1a487d4cab947b3f1e65
Alejandro <alopezlago@cs.hmc.edu>
2022-11-01 22:48:09 -0700

    Merged PR 257: v1.10.2 - Fix bug Tossup/Bonus scrollbar doesn't reset when a new question is loadedcmd (#178)
    - Fix bug Tossup/Bonus scrollbar doesn't reset when a new question is loaded
    - Bump version to 1.10.2

--- src/components/BonusQuestion.tsx
+++ src/components/BonusQuestion.tsx
@@ -18,6 +18,8 @@ const throwOutQuestionPrompt: ICancelButtonPrompt = {
     message: "Click OK to throw out the bonus. To undo this, click on the X next to its event in the Event Log.",
 };
 
+let bonusQuestionTextIdCounter = 0;
+
 export const BonusQuestion = observer(function BonusQuestion(props: IBonusQuestionProps) {
     const fontSize: number = props.appState.uiState.questionFontSize;
     const classes: IBonusQuestionClassNames = getClassNames(fontSize, !props.inPlay);
@@ -28,6 +30,27 @@ export const BonusQuestion = observer(function BonusQuestion(props: IBonusQuesti
         () => PacketState.getBonusWords(props.bonus.leadin, props.appState.game.gameFormat),
         [props.bonus.leadin, props.appState.game.gameFormat]
     );
+    const [lastBonus, setLastBonus] = React.useState(props.bonus);
+
+    // Set the ID and bump it up for the next item
+    const [bonusId] = React.useState(bonusQuestionTextIdCounter);
+    React.useEffect(() => {
+        bonusQuestionTextIdCounter++;
+    }, []);
+
+    const bonusQuestionTextId = `bonusQuestionText${bonusId}`;
+
+    const hasBonusChanged = lastBonus !== props.bonus;
+    if (hasBonusChanged) {
+        setLastBonus(props.bonus);
+
+        // Because the bonus text has the scrollbar, and StackItem doesn't have a ref field, we have to get it manually
+        // from the DOM. We can't use a class name because it keeps changing, so we have to use an ID.
+        const bonusText: HTMLElement | null = document.getElementById(bonusQuestionTextId);
+        if (bonusText != null) {
+            bonusText.scrollTop = 0;
+        }
+    }
 
     const parts: JSX.Element[] = props.bonus.parts.map((bonusPartProps, index) => {
         return (
@@ -53,7 +76,7 @@ export const BonusQuestion = observer(function BonusQuestion(props: IBonusQuesti
         <div className={classes.bonusContainer}>
             <BonusProtestDialog appState={props.appState} bonus={props.bonus} cycle={props.cycle} />
             <Stack horizontal={true}>
-                <StackItem className={classes.bonusText}>
+                <StackItem id={bonusQuestionTextId} className={classes.bonusText}>
                     <FocusZone as="div" shouldRaiseClicks={true} direction={FocusZoneDirection.vertical}>
                         <FormattedText className={classes.bonusLeadin} segments={formattedLeadin} />
                         {parts}
